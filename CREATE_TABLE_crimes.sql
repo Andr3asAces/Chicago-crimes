12abec563cb926345ab4f09a03d52a37407d13f8
Andr3asaces <a.belegratis@yahoo.com>
2023-05-01 14:25:23 +0100

    first commit

---
+++ CREATE_TABLE_crimes.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE crimes(
+id CHAR(10) PRIMARY KEY,	
+case_number CHAR(10),	
+date_ DATE,	
+block VARCHAR(50),
+iucr VARCHAR(8),
+primary_type VARCHAR, 	
+description VARCHAR, 	
+location_description VARCHAR, 	
+arrest	BOOLEAN,
+domestic BOOLEAN,	
+beat	INTEGER,
+district SMALLINT,	
+ward	SMALLINT,
+community_area	SMALLINT,
+fbi_code VARCHAR(5),
+x_coordinate NUMERIC,
+y_coordinate NUMERIC,	
+year_ INTEGER,	
+updated_on DATE,		
+latitude NUMERIC,	
+longitude NUMERIC,	
+location_ VARCHAR
+                  );
+-- \copy crimes from 'C:\Users\ANDREAS\Desktop\Data Analyst Revamp\IBM Data Scientist\DS_C06\FINAL ASSIGNMENT\crimes.csv' delimiter ',' csv header;
+
+
+
---
+++ CREATE_TABLE_indicators.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE indicators(
+ca INTEGER,	
+community_area_name VARCHAR(50),	
+percent_of_housing_crowded DECIMAL,
+percent_households_below_poverty DECIMAL,	
+percent_aged_16_unemployed DECIMAL,	
+percent_aged_25_without_high_school_diploma DECIMAL,		
+percent_aged_under_18_or_over_64 DECIMAL,		
+per_capita_income_ NUMERIC,	
+hardship_index INTEGER
+                       );                      
+
+
+-- \copy indicators from '...\FINAL ASSIGNMENT\indicators.csv' delimiter ',' csv header;
+
+
